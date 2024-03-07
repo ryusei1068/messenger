5f1f25ca687272467f132f9135f3c2f34d6882ab
Ryusei Nohara <nhrryi@gmail.com>
2024-03-07 23:28:12 +0900

    splited files

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,50 @@
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::net::{SocketAddr, UdpSocket};
+use std::net::UdpSocket;
 use std::str;
 use std::sync::mpsc;
 use std::thread;
 
+mod method;
+use method::Method;
+mod request;
+use request::Request;
+mod room;
+use room::Room;
+mod client;
+use client::Client;
+mod utils;
+
 const UDP_SERVER_ADDRESS: &str = "127.0.0.1:8080";
 const MSG_SIZE: usize = 4096;
 
-#[derive(Debug)]
-enum Method {
-    Join,
-    Send,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Request {
-    method: String,
-    from: String,
-    to: String,
-    text: String,
-}
-
-#[derive(Debug)]
-struct Client {
-    name: String,
-    socket_addr: SocketAddr,
-}
-
-#[derive(Debug)]
-struct Room {
-    udp_socket: UdpSocket,
-    clients: HashMap<String, Client>,
-}
-
-impl Room {
-    fn join(&mut self, name: String, client: Client) {
-        self.clients.insert(name, client);
-    }
-
-    fn broadcast(&self, request: Request) {
-        for client in self.clients.values() {
-            if client.name == request.from {
-                continue;
-            }
-            self.sender(client.socket_addr, request.text.as_bytes());
-        }
-    }
-
-    fn send_private_msg(&self, request: Request) {
-        if let Some(client) = self.get_client_by_name(request.to) {
-            self.sender(client.socket_addr, request.text.as_bytes());
-        }
-    }
-
-    fn get_client_by_name(&self, name: String) -> Option<&Client> {
-        self.clients.get(&name)
-    }
-
-    fn sender(&self, socket_addr: SocketAddr, byte: &[u8]) {
-        match self.udp_socket.send_to(byte, socket_addr) {
-            Ok(_) => {
-                println!("\nsucceeded");
-            }
-            Err(e) => {
-                println!("\ncloud not send a request.text: {:?}", e);
-            }
-        }
-    }
-}
-
-fn map_method(method: &String) -> Option<Method> {
-    match method.as_str() {
-        "1" => Some(Method::Join),
-        "2" => Some(Method::Send),
-        _ => None,
-    }
-}
-
-fn parse(buf: &[u8], socket_addr: SocketAddr) -> Result<(Request, Client), String> {
-    let req_byte = match std::str::from_utf8(&buf) {
-        Ok(req_byte) => req_byte,
-        Err(e) => {
-            println!("Error converting bytes to string: {}", e);
-            ""
-        }
-    };
-
-    if req_byte.len() == 0 {
-        return Err("failed to parse error".into());
-    }
-
-    match serde_json::from_str::<Request>(&req_byte) {
-        Ok(req) => {
-            let from = req.from.clone();
-            Ok((
-                req,
-                Client {
-                    name: from,
-                    socket_addr: socket_addr,
-                },
-            ))
-        }
-        Err(e) => Err(format!("failed to deserialize {:?}", e)),
-    }
-}
-
 fn main() {
-    let socket = UdpSocket::bind(UDP_SERVER_ADDRESS).expect("could not bind UdpSocket");
-    let socket_clone = socket.try_clone().expect("could not clone of UdpSocket");
+    let udp_socket = UdpSocket::bind(UDP_SERVER_ADDRESS).expect("could not bind UdpSocket");
+    let udp_socket_clone = udp_socket
+        .try_clone()
+        .expect("could not clone of UdpSocket");
 
     let (tx, rx) = mpsc::channel::<(Request, Client)>();
 
     let mut buf = [0; MSG_SIZE];
 
-    let mut room = Room {
-        udp_socket: socket_clone,
-        clients: HashMap::new(),
-    };
+    let mut room = Room::new(udp_socket, HashMap::new());
 
     thread::spawn(move || {
         while let Ok(req) = rx.recv() {
-            if let Some(ref method) = map_method(&req.0.method) {
+            if let Some(ref method) = utils::map_method(&req.0.get_method()) {
                 match method {
                     Method::Join => {
                         println!("\njoin: {:?}", req.1);
-                        let name = req.1.name.clone();
+                        let name = req.1.get_name();
                         room.join(name, req.1);
                     }
                     Method::Send => {
                         println!("\nsend: {:?}", req.0);
                         room.broadcast(req.0);
+                    }
+                    Method::PvtMsg => {
+                        println!("\nsend: {:?}", req.0);
+                        room.private_msg(req.0);
                     }
                 }
             }
@@ -138,10 +52,10 @@ fn main() {
     });
 
     loop {
-        match socket.recv_from(&mut buf) {
-            Ok((buf_size, socket_addr)) => {
+        match udp_socket_clone.recv_from(&mut buf) {
+            Ok((buf_size, udp_socket_addr)) => {
                 let buf = &mut buf[..buf_size];
-                if let Ok(parsed_request) = parse(&buf, socket_addr) {
+                if let Ok(parsed_request) = utils::parse(&buf, udp_socket_addr) {
                     let _ = tx.send(parsed_request);
                 }
             }
